a06a80da26c8a11227eaf330627f25c876d5695b
rsaifi12 <112189558+rsaifi12@users.noreply.github.com>
2023-09-17 15:47:53 +0530

    Update install-terraform.sh

--- install-terraform.sh
+++ install-terraform.sh
@@ -1,47 +1,20 @@
+
+
+
+This is original 
 #!/bin/bash
 
 # fail on any error
 set -eu
 
-# Check if CLIENT_TOOLS_DOWNLOAD_DISABLED is set to true
-if [ "${CLIENT_TOOLS_DOWNLOAD_DISABLED}" = "true" ]; then
-  echo "Client tools download is disabled. Skipping installation."
-  exit 0
-fi
+# install yum-config-manager to manage your repositories
+sudo yum install -y yum-utils
 
-# Check if INSTALL_CLIENT_TOOLS_IN_BACKGROUND is set to true
-if [ "${INSTALL_CLIENT_TOOLS_IN_BACKGROUND}" = "true" ]; then
-  echo "Installing client tools in the background..."
-  # Add your installation commands for client tools here
-else
-  # install yum-config-manager to manage your repositories
-  sudo yum install -y yum-utils
+# use yum-config-manager to add the official HashiCorp Linux repository
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 
-  # use yum-config-manager to add the official HashiCorp Linux repository
-  sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# install terraform
+sudo yum -y install terraform
 
-  # install terraform
-  sudo yum -y install terraform
-
-  # verify terraform is installed
-  terraform --version
-fi
-
-
-# This is original 
-# #!/bin/bash
-
-# # fail on any error
-# set -eu
-
-# # install yum-config-manager to manage your repositories
-# sudo yum install -y yum-utils
-
-# # use yum-config-manager to add the official HashiCorp Linux repository
-# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-
-# # install terraform
-# sudo yum -y install terraform
-
-# # verify terraform is installed
-# terraform --version
+# verify terraform is installed
+terraform --version
