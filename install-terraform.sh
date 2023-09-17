8ccf4d06ac7aba1c35fa8ec026d44d4b7b67681c
rsaifi12 <112189558+rsaifi12@users.noreply.github.com>
2023-09-17 14:03:33 +0530

    Update install-terraform.sh

--- install-terraform.sh
+++ install-terraform.sh
@@ -14,3 +14,21 @@ sudo yum -y install terraform
 
 # verify terraform is installed
 terraform --version
+
+# This is original 
+# #!/bin/bash
+
+# # fail on any error
+# set -eu
+
+# # install yum-config-manager to manage your repositories
+# sudo yum install -y yum-utils
+
+# # use yum-config-manager to add the official HashiCorp Linux repository
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+
+# # install terraform
+# sudo yum -y install terraform
+
+# # verify terraform is installed
+# terraform --version
